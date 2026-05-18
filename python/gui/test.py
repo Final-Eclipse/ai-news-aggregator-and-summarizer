155b303047655d3b2a26bfa2414fa81175d2a874
Final-Eclipse <AnimeAndManga1@proton.me>
2026-05-18 03:10:12 -0400

    Test contacting Ollama localhost and added tentative roadmap.

--- python/gui/test.py
+++ python/gui/test.py
@@ -1,3 +1,9 @@
 from PyQt5 import QtCore
 
-print("hi")
+# Create a GUI to display the article titles that follow the descriptions the user has set.
+# Send a GET request for all of the articles for the specified endpoint.
+# User will type the article's URL which will send a POST request to Spring Boot to scrape the article's text and update an endpoint like "/article-text".
+# When the summarize button is clicked, send a GET request to C#.
+# C# will send a GET request to Spring Boot for the article text and send another request to a local model using OllamaSharp.
+# Send a POST request back to Spring Boot at "/summary" with the article's summary.
+# In Python, send a GET request to Spring Boot to retrieve the summary and display it.
--- python/request_api/get_data.py
+++ python/request_api/get_data.py
@@ -1,5 +1,21 @@
-import requests
+import requests, json
 
-request = requests.get("http://localhost:4567/EverythingEndpoint")
+# request = requests.get("http://localhost:4567/EverythingEndpoint")
+# request = requests.get("http://localhost:8080/api/v1/news/summary")
+payload = {
+    "model": "llama3.1:8b",
+    # "messages": [{"role": "user", "content": "hello"}]
+    "prompt": "Give me a brief but interesting fact.", 
+    "stream": True
+}
+request = requests.post("http://localhost:11434/api/generate", json=payload)
+response = request.text
+split_response = response.split("\"response\":\"")
+# "\",\"done\""
+new_string = []
+for index, x in enumerate(split_response):
+    if index % 2 == 1:
+        new_string.append(x.split("\",\"done\""))
+        print(x.split("\",\"done\""))
+        print()
 
-print(request.text)
