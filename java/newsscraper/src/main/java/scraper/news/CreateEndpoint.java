190ed922c23cccd07ed89ffcd9493bbe80a38127
Final-Eclipse <AnimeAndManga1@proton.me>
2026-05-27 00:58:16 -0400

    Separate create method into multiple

--- java/newsscraper/src/main/java/scraper/news/CreateEndpoint.java
+++ java/newsscraper/src/main/java/scraper/news/CreateEndpoint.java
@@ -4,70 +4,65 @@ import java.util.HashMap;
 
 public class CreateEndpoint 
 {
-    private static Endpoint determineEndpoint(HashMap<String, String> endpointDataHashMap)
+    public static Endpoint create(HashMap<String, String> endpointDataHashMap)
     {
         switch (endpointDataHashMap.get("endpoint"))
         {
             case "everything":
-                return new EverythingEndpoint.Builder() 
-                    .q(endpointDataHashMap.getOrDefault("q", null))
-                    .searchIn(endpointDataHashMap.getOrDefault("searchIn", null))
-                    .sources(endpointDataHashMap.getOrDefault("sources", null))
-                    .domains(endpointDataHashMap.getOrDefault("domains", null))
-                    .excludeDomains(endpointDataHashMap.getOrDefault("excludeDomains", null))
-                    .from(endpointDataHashMap.getOrDefault("from", null))
-                    .to(endpointDataHashMap.getOrDefault("to", null))
-                    .language(endpointDataHashMap.getOrDefault("language", null))
-                    .sortBy(endpointDataHashMap.getOrDefault("sortBy", null))
-                    .pageSize(endpointDataHashMap.getOrDefault("pageSize", null))
-                    .page(endpointDataHashMap.getOrDefault("page", null))
-                    .build();
-
-            case "top-headlines":
-                // Assigns a default value to category.
-                if (endpointDataHashMap.get("category").equals("null"))
-                {
-                    endpointDataHashMap.put("category", "general");
-                }
+                return createEverythingEndpoint(endpointDataHashMap);
                 
-                return new TopHeadlinesEndpoint.Builder()
-                    .country(endpointDataHashMap.getOrDefault("country", null))
-                    .category(endpointDataHashMap.getOrDefault("category", null))
-                    .sources(endpointDataHashMap.getOrDefault("sources", null))
-                    .q(endpointDataHashMap.getOrDefault("q", null))
-                    .pageSize(endpointDataHashMap.getOrDefault("pageSize", null))
-                    .page(endpointDataHashMap.getOrDefault("page", null))
-                    .build();
-
+            case "top-headlines":
+                return createTopHeadlinesEndpoint(endpointDataHashMap);
+                
             case "top-headlines/sources":
-                return new SourcesEndpoint.Builder()
-                    .category(endpointDataHashMap.getOrDefault("category", null))
-                    .language(endpointDataHashMap.getOrDefault("language", null))
-                    .country(endpointDataHashMap.getOrDefault("country", null))
-                    .build();
+                return createSourcesEndpoint(endpointDataHashMap);
 
             default:
                 throw new IllegalArgumentException("Invalid endpoint type.");
         }
     }
 
-    public static Endpoint create(HashMap<String, String> endpointDataHashMap)
+    private static EverythingEndpoint createEverythingEndpoint(HashMap<String, String> endpointDataHashMap)
     {
-        return determineEndpoint(endpointDataHashMap);
+        return new EverythingEndpoint.Builder() 
+            .q(endpointDataHashMap.getOrDefault("q", null))
+            .searchIn(endpointDataHashMap.getOrDefault("searchIn", null))
+            .sources(endpointDataHashMap.getOrDefault("sources", null))
+            .domains(endpointDataHashMap.getOrDefault("domains", null))
+            .excludeDomains(endpointDataHashMap.getOrDefault("excludeDomains", null))
+            .from(endpointDataHashMap.getOrDefault("from", null))
+            .to(endpointDataHashMap.getOrDefault("to", null))
+            .language(endpointDataHashMap.getOrDefault("language", null))
+            .sortBy(endpointDataHashMap.getOrDefault("sortBy", null))
+            .pageSize(endpointDataHashMap.getOrDefault("pageSize", null))
+            .page(endpointDataHashMap.getOrDefault("page", null))
+            .build();
     }
 
-    // public EverythingEndpoint create()
-    // {
-        
-    // }
+    private static TopHeadlinesEndpoint createTopHeadlinesEndpoint(HashMap<String, String> endpointDataHashMap)
+    {
+        // Assigns a default value to category.
+        if (endpointDataHashMap.get("category") == null)
+        {
+            endpointDataHashMap.put("category", "general");
+        }
 
-    // public TopHeadlinesEndpoint create()
-    // {
+        return new TopHeadlinesEndpoint.Builder()
+            .country(endpointDataHashMap.getOrDefault("country", null))
+            .category(endpointDataHashMap.getOrDefault("category", "general"))
+            .sources(endpointDataHashMap.getOrDefault("sources", null))
+            .q(endpointDataHashMap.getOrDefault("q", null))
+            .pageSize(endpointDataHashMap.getOrDefault("pageSize", null))
+            .page(endpointDataHashMap.getOrDefault("page", null))
+            .build();
+    }
 
-    // }
-
-    // public SourcesEndpoint create()
-    // {
-
-    // }
+    private static SourcesEndpoint createSourcesEndpoint(HashMap<String, String> endpointDataHashMap)
+    {
+        return new SourcesEndpoint.Builder()
+            .category(endpointDataHashMap.getOrDefault("category", null))
+            .language(endpointDataHashMap.getOrDefault("language", null))
+            .country(endpointDataHashMap.getOrDefault("country", null))
+            .build();
+    }
 }
