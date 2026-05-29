0d532cde39499f102ca19698214183af77f1371a
Final-Eclipse <AnimeAndManga1@proton.me>
2026-05-29 04:11:53 -0400

    Add new using directives

---
+++ csharp/ai-news-aggregator-and-summarizer/ai-news-aggregator-and-summarizer/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ai_news_aggregator_and_summarizer;
+
+using OllamaSharp;
+// https://github.com/awaescher/OllamaSharp/tree/main
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{   
+    public static async Task Main(string[] args)
+    {
+        // GlobalHttpClient httpClient = new GlobalHttpClient();
+        
+        // RestApiClient restApiClient = new RestApiClient(GlobalHttpClient.httpClient);
+        // restApiClient.PrintResponse();
+
+//         LocalModelClient localModelClient = new LocalModelClient(GlobalHttpClient.httpClient);
+//         await localModelClient.SendMessage($"Can you summarize this article. \n{articleText}");
+//         Console.Write(localModelClient.GetResponse());
+
+
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+
+        // Create new csproj for asp net core.
+        // Or have two csprojs in the csharp folder.
+    }
+}
