1ce3c841af020adf6d146fc762c68dc64d1db8ae
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-01 05:32:16 -0400

    Remove unnecessary comments and code

--- csharp/ai-news-aggregator-and-summarizer/Controllers/SummaryController.cs
+++ csharp/ai-news-aggregator-and-summarizer/Controllers/SummaryController.cs
@@ -7,21 +7,9 @@ namespace ai_news_aggregator_and_summarizer.Controllers;
 [Route("[controller]")]
 public class SummaryController : ControllerBase
 {
-    // public SummaryController()
-    // {
-    
-    // }
-
-    public string GetSummary(string articleText)
+    [HttpPost]
+    public string PostArticleText([FromBody] string articleText)
     {
         return SummaryService.Get(articleText).Result.summaryText;
     }
-    
-    // GET by Id action
-
-    // POST action
-
-    // PUT action
-
-    // DELETE action
 }
