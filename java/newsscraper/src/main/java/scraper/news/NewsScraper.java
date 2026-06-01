bf61214dd644a33e105bc40b99b623942bdb526c
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-01 05:42:03 -0400

    Refactor and add new methods
    - Create static HttpClient variable.
    - Create handleErrorCodes() to throw a RuntimeException for error codes
      other than 200.
    - Create getSummarization() to contact ASP.NET Core localhost and return
      a summarization for the String parameter articleText.

--- java/newsscraper/src/main/java/scraper/news/NewsScraper.java
+++ java/newsscraper/src/main/java/scraper/news/NewsScraper.java
@@ -4,14 +4,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+
+import org.springframework.boot.jackson.autoconfigure.JacksonProperties.Json;
+
+import tools.jackson.databind.ObjectMapper;
+
 import java.net.URI;
 
 public class NewsScraper 
 {
-    private HttpClient client = HttpClient.newHttpClient();
-    private String apiKey = System.getenv("NEWSAPI_API_KEY");
+    private static HttpClient client = HttpClient.newHttpClient();
+    private static String apiKey = System.getenv("NEWSAPI_API_KEY");
 
-    public String getResponse(String apiEndpointUrl)
+    public static String getResponse(String apiEndpointUrl)
     {   
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(apiEndpointUrl))
@@ -19,12 +24,34 @@ public class NewsScraper
             .build();
 
         HttpResponse<String> response = client.sendAsync(request, BodyHandlers.ofString()).join();
+        handleErrorCodes(response);
 
+        return response.body();
+    }    
+
+    public static String getSummarization(String articleText)
+    {
+        // Serializes articleText as a JSON String.
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(articleText);
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:5172/summary"))
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+            .build();
+        
+        HttpResponse<String> response = client.sendAsync(request, BodyHandlers.ofString()).join();
+        handleErrorCodes(response);
+        
+        return response.body();
+    }
+
+    private static void handleErrorCodes(HttpResponse<String> response)
+    {
         if (response.statusCode() != 200)
         {
             throw new RuntimeException("Error Code " + response.statusCode());
         }
-
-        return response.body();
-    }    
+    }
 }
