faead20d3e7d2bdb68a1bcfe54be5fb8b605346f
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-01 05:58:07 -0400

    Resolve merge conflict

--- csharp/ai-news-aggregator-and-summarizer/Services/SummaryService.cs
+++ csharp/ai-news-aggregator-and-summarizer/Services/SummaryService.cs
@@ -5,19 +5,29 @@ namespace ai_news_aggregator_and_summarizer.Services;
 
 public static class SummaryService
 {
-    static string? summaryText;
-    static readonly Uri uri = new Uri("http://localhost:11434");
-    static readonly OllamaApiClient ollama = new OllamaApiClient(uri);
+    public static string? summaryText;
+    public static readonly Uri uri = new Uri("http://localhost:11434");
+    public static readonly OllamaApiClient ollama = new OllamaApiClient(uri);
+    // public static string selectedModel = "";
 
     static SummaryService()
+<<<<<<< Updated upstream
     {    
         // Throws AggregateException and HttpRequestException if selected model does not exist on the user's computer.
         ollama.SelectedModel = "huihui_ai/deepseek-r1-abliterated:8b";  // Have selected model as query parameter?
+=======
+    {   
+        // Throws AggregateException and HttpRequestException if selected model does not exist on the user's computer.
+        ollama.SelectedModel = "huihui_ai/deepseek-r1-abliterated:8b";  // Have selected model as query parameter?
+        // ollama.SelectedModel = selectedModel;
+        // System.Console.WriteLine(ollama.SelectedModel);
+>>>>>>> Stashed changes
     }
 
     // Contacts local ollama model and Initializes summaryText;
     public static async Task Summarize(string articleText)
     {
+        // await foreach (var stream in ollama.GenerateAsync($"Can you summarize this article? {articleText}"))
         await foreach (var stream in ollama.GenerateAsync(articleText))
         {
             summaryText += stream?.Response;
