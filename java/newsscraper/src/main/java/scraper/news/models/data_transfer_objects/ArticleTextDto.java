7c1f7de4f50d874ec6138f6bab3ff85ec39aa493
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-10 05:34:37 -0400

    Rename directory

---
+++ java/newsscraper/src/main/java/scraper/news/models/data_transfer_objects/ArticleTextDto.java
@@ -0,0 +1,16 @@
+package scraper.news.models.dtos;
+
+public class ArticleTextDto 
+{
+    private String articleText;
+
+    public String getArticleText()
+    {
+        return articleText;
+    }
+
+    public void setArticleText(String articleText)
+    {
+        this.articleText = articleText;
+    }
+}
