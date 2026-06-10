626ba533c48f7e886e63ff011439dd4c8e4ba474
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-10 03:47:47 -0400

    Change package to match new directory

--- java/newsscraper/src/main/java/scraper/news/controllers/EndpointController.java
+++ java/newsscraper/src/main/java/scraper/news/controllers/EndpointController.java
@@ -1,4 +1,4 @@
-package scraper.news.newscontroller;
+package scraper.news.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
