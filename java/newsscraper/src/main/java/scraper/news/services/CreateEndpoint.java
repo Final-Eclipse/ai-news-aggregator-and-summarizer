096b6de251fad7287f297079aa8b534198453fae
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-13 03:04:09 -0400

    Add Javadocs to each method

--- java/newsscraper/src/main/java/scraper/news/services/CreateEndpoint.java
+++ java/newsscraper/src/main/java/scraper/news/services/CreateEndpoint.java
@@ -7,6 +7,12 @@ import scraper.news.services.newsapi_endpoints.TopHeadlinesEndpoint;
 
 public class CreateEndpoint 
 {
+    /**
+     * Determines the endpoint type, calls its creation method, and returns the EndpointService result.
+     * 
+     * @param endpointData EndpointDto that contains data describing the endpoint and its query parameters.
+     * @return EndpointService, an abstract class containing the apiEndpointUrl and related methods. 
+     */
     public static EndpointService create(EndpointDto endpointData)
     {
         switch (endpointData.getEndpoint())
@@ -25,6 +31,12 @@ public class CreateEndpoint
         }
     }
 
+    /**
+     * Constructs and returns an EverythingEndpoint.
+     * 
+     * @param endpointData EndpointDto that contains data describing the endpoint and its query parameters.
+     * @return EverythingEndpoint, a class that extends EndpointService.
+     */
     private static EverythingEndpoint createEverythingEndpoint(EndpointDto endpointData)
     {
         return new EverythingEndpoint.Builder() 
@@ -42,6 +54,12 @@ public class CreateEndpoint
             .build();
     }
 
+    /**
+     * Constructs and returns a TopHeadlinesEndpoint.
+     * 
+     * @param endpointData EndpointDto that contains data describing the endpoint and its query parameters.
+     * @return TopHeadlinesEndpoint, a class that extends EndpointService.
+     */
     private static TopHeadlinesEndpoint createTopHeadlinesEndpoint(EndpointDto endpointData)
     {
         return new TopHeadlinesEndpoint.Builder()
@@ -54,6 +72,12 @@ public class CreateEndpoint
             .build();
     }
 
+    /**
+     * Constructs and returns a SourcesEndpoint.
+     * 
+     * @param endpointData EndpointDto that contains data describing the endpoint and its query parameters.
+     * @return SourcesEndpoint, a class that extends EndpointService.
+     */
     private static SourcesEndpoint createSourcesEndpoint(EndpointDto endpointData)
     {
         return new SourcesEndpoint.Builder()
@@ -62,72 +86,4 @@ public class CreateEndpoint
             .country(endpointData.getCountry())
             .build();
     }
-
-    // ========================
-    // ========================
-    // ========================
-    // ========================
-    // ========================
-
-    // public static Endpoint create(HashMap<String, String> endpointDataHashMap)
-    // {
-    //     switch (endpointDataHashMap.get("endpoint"))
-    //     {
-    //         case "everything":
-    //             return createEverythingEndpoint(endpointDataHashMap);
-                
-    //         case "top-headlines":
-    //             return createTopHeadlinesEndpoint(endpointDataHashMap);
-                
-    //         case "top-headlines/sources":
-    //             return createSourcesEndpoint(endpointDataHashMap);
-
-    //         default:
-    //             throw new IllegalArgumentException("Invalid endpoint type.");
-    //     }
-    // }
-
-    // private static EverythingEndpoint createEverythingEndpoint(HashMap<String, String> endpointDataHashMap)
-    // {
-    //     return new EverythingEndpoint.Builder() 
-    //         .q(endpointDataHashMap.getOrDefault("q", null))
-    //         .searchIn(endpointDataHashMap.getOrDefault("searchIn", null))
-    //         .sources(endpointDataHashMap.getOrDefault("sources", null))
-    //         .domains(endpointDataHashMap.getOrDefault("domains", null))
-    //         .excludeDomains(endpointDataHashMap.getOrDefault("excludeDomains", null))
-    //         .from(endpointDataHashMap.getOrDefault("from", null))
-    //         .to(endpointDataHashMap.getOrDefault("to", null))
-    //         .language(endpointDataHashMap.getOrDefault("language", null))
-    //         .sortBy(endpointDataHashMap.getOrDefault("sortBy", null))
-    //         .pageSize(endpointDataHashMap.getOrDefault("pageSize", null))
-    //         .page(endpointDataHashMap.getOrDefault("page", null))
-    //         .build();
-    // }
-
-    // private static TopHeadlinesEndpoint createTopHeadlinesEndpoint(HashMap<String, String> endpointDataHashMap)
-    // {
-    //     // Assigns a default value to category.
-    //     if (endpointDataHashMap.get("category") == null)
-    //     {
-    //         endpointDataHashMap.put("category", "general");
-    //     }
-
-    //     return new TopHeadlinesEndpoint.Builder()
-    //         .country(endpointDataHashMap.getOrDefault("country", null))
-    //         .category(endpointDataHashMap.getOrDefault("category", "general"))
-    //         .sources(endpointDataHashMap.getOrDefault("sources", null))
-    //         .q(endpointDataHashMap.getOrDefault("q", null))
-    //         .pageSize(endpointDataHashMap.getOrDefault("pageSize", null))
-    //         .page(endpointDataHashMap.getOrDefault("page", null))
-    //         .build();
-    // }
-
-    // private static SourcesEndpoint createSourcesEndpoint(HashMap<String, String> endpointDataHashMap)
-    // {
-    //     return new SourcesEndpoint.Builder()
-    //         .category(endpointDataHashMap.getOrDefault("category", null))
-    //         .language(endpointDataHashMap.getOrDefault("language", null))
-    //         .country(endpointDataHashMap.getOrDefault("country", null))
-    //         .build();
-    // }
 }
