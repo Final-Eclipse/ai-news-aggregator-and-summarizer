475aa21432743bdc3f69ecb68e84cfc705cf9cd6
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-13 04:25:14 -0400

    Add Javadocs to each method

--- java/newsscraper/src/main/java/scraper/news/services/HttpService.java
+++ java/newsscraper/src/main/java/scraper/news/services/HttpService.java
@@ -4,6 +4,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.HashMap;
 
 import tools.jackson.databind.ObjectMapper;
 
@@ -11,9 +12,15 @@ import java.net.URI;
 
 public class HttpService 
 {
-    private static HttpClient client = HttpClient.newHttpClient();
+    private final static HttpClient client = HttpClient.newHttpClient();
     private static String apiKey = System.getenv("NEWSAPI_API_KEY");
 
+    /**
+     * Makes an HTTP GET request to NewsAPI and returns the result as a String.
+     * 
+     * @param apiEndpointUrl String URL pointing to one of NewsAPI's endpoints.
+     * @return String representation of the JSON result.
+     */
     public static String getResponse(String apiEndpointUrl)
     {   
         HttpRequest request = HttpRequest.newBuilder()
@@ -24,9 +31,31 @@ public class HttpService
         HttpResponse<String> response = client.sendAsync(request, BodyHandlers.ofString()).join();
         handleErrorCodes(response);
 
+        // Use global objectMapper for this file?
+        // Figure out how to return HashMap representation of the JSON result instead of a String.
+
+        // ObjectMapper objectMapper = new ObjectMapper();
+        // HashMap<String, Object> jsonHashMap = objectMapper.readValue(response.body(), HashMap.class);
+        // System.out.println(jsonHashMap.get("articles"));
+
+        // String jsonString = (String) jsonHashMap.get("articles");
+        // String[] articlesArray = objectMapper.readValue(jsonString, String[].class);
+        // System.out.println(articlesArray);
+
+        // String[] articlesArray = (String[]) jsonHashMap.get("articles");
+        // System.out.println(articlesArray[0]);
+        // String jsonString = objectMapper.writeValueAsString(response.body());
+        // return jsonString;
+
         return response.body();
     }    
 
+    /**
+     * Makes a HTTP POST request to localhost:5172 (ASP.NET Core) and returns a summary of the given article.
+     * 
+     * @param articleText String of an article's content.
+     * @return String summarization of the article.
+     */
     public static String getSummarization(String articleText)
     {
         // Serializes articleText as a JSON String.
@@ -45,6 +74,12 @@ public class HttpService
         return response.body();
     }
 
+    /**
+     * Handles non-200 error codes from HTTP responses.
+     * 
+     * @throws RuntimeException if the response's status code is not 200.
+     * @param response HttpResponse<String> that is used to determine the HTTP response's status code.
+     */
     private static void handleErrorCodes(HttpResponse<String> response)
     {
         if (response.statusCode() != 200)
