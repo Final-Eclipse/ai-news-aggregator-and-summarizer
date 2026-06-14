cb3ae613fe73bd80f4a7f0df409883012c435c7a
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-14 05:28:39 -0400

    Create GET methods for each NewsAPI endpoint

--- java/newsscraper/src/main/java/scraper/news/services/HttpService.java
+++ java/newsscraper/src/main/java/scraper/news/services/HttpService.java
@@ -4,16 +4,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.HashMap;
+import java.net.URI;
 
 import tools.jackson.databind.ObjectMapper;
 
-import java.net.URI;
+import scraper.news.models.data_transfer_objects.EverythingDto;
+import scraper.news.models.data_transfer_objects.SourcesDto;
+import scraper.news.models.data_transfer_objects.TopHeadlinesDto;
 
 public class HttpService 
 {
     private final static HttpClient client = HttpClient.newHttpClient();
     private static String apiKey = System.getenv("NEWSAPI_API_KEY");
+    private final static ObjectMapper objectMapper = new ObjectMapper();
 
     /**
      * Makes an HTTP GET request to NewsAPI and returns the result as a String.
@@ -21,7 +24,7 @@ public class HttpService
      * @param apiEndpointUrl String URL pointing to one of NewsAPI's endpoints.
      * @return String representation of the JSON result.
      */
-    public static String getResponse(String apiEndpointUrl)
+    public static String getNewsApiResponse(String apiEndpointUrl)
     {   
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(apiEndpointUrl))
@@ -30,25 +33,29 @@ public class HttpService
 
         HttpResponse<String> response = client.sendAsync(request, BodyHandlers.ofString()).join();
         handleErrorCodes(response);
-
-        // Use global objectMapper for this file?
-        // Figure out how to return HashMap representation of the JSON result instead of a String.
-
-        // ObjectMapper objectMapper = new ObjectMapper();
-        // HashMap<String, Object> jsonHashMap = objectMapper.readValue(response.body(), HashMap.class);
-        // System.out.println(jsonHashMap.get("articles"));
-
-        // String jsonString = (String) jsonHashMap.get("articles");
-        // String[] articlesArray = objectMapper.readValue(jsonString, String[].class);
-        // System.out.println(articlesArray);
-
-        // String[] articlesArray = (String[]) jsonHashMap.get("articles");
-        // System.out.println(articlesArray[0]);
-        // String jsonString = objectMapper.writeValueAsString(response.body());
-        // return jsonString;
-
         return response.body();
     }    
+
+    public static EverythingDto getEverythingResponse(String apiEndpointUrl)
+    {
+        String responseBody = getNewsApiResponse(apiEndpointUrl);
+        EverythingDto everythingResponse = objectMapper.readValue(responseBody, EverythingDto.class);
+        return everythingResponse;
+    }
+
+    public static TopHeadlinesDto getTopHeadlinesResponse(String apiEndpointUrl)
+    {
+        String responseBody = getNewsApiResponse(apiEndpointUrl);
+        TopHeadlinesDto topHeadlinesResponse = objectMapper.readValue(responseBody, TopHeadlinesDto.class);
+        return topHeadlinesResponse;
+    }
+
+    public static SourcesDto getSourcesResponse(String apiEndpointUrl)
+    {
+        String responseBody = getNewsApiResponse(apiEndpointUrl);
+        SourcesDto sourcesResponse = objectMapper.readValue(responseBody, SourcesDto.class);
+        return sourcesResponse;
+    }
 
     /**
      * Makes a HTTP POST request to localhost:5172 (ASP.NET Core) and returns a summary of the given article.
@@ -59,7 +66,6 @@ public class HttpService
     public static String getSummarization(String articleText)
     {
         // Serializes articleText as a JSON String.
-        ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = objectMapper.writeValueAsString(articleText);
 
         HttpRequest request = HttpRequest.newBuilder()
