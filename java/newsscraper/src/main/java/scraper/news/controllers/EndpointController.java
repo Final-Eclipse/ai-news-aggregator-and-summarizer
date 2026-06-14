ab8de535173e6607fca05d1f17b3b54d1b5a3845
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-14 05:37:45 -0400

    Create GetMapping methods for each NewsAPI endpoint

--- java/newsscraper/src/main/java/scraper/news/controllers/EndpointController.java
+++ java/newsscraper/src/main/java/scraper/news/controllers/EndpointController.java
@@ -7,90 +7,138 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import scraper.news.services.CreateEndpointService;
-import scraper.news.services.newsapi_endpoints.EverythingEndpointService;
-import scraper.news.services.newsapi_endpoints.TopHeadlinesEndpointService;
-import scraper.news.services.newsapi_endpoints.SourcesEndpointService;
+// import scraper.news.services.newsapi_endpoints.EverythingEndpointService;
+// import scraper.news.services.newsapi_endpoints.TopHeadlinesEndpointService;
+// import scraper.news.services.newsapi_endpoints.SourcesEndpointService;
 import scraper.news.services.HttpService;
 import scraper.news.services.BaseEndpointService;
 import scraper.news.models.data_transfer_objects.ArticleTextDto;
 import scraper.news.models.data_transfer_objects.EndpointDto;
+import scraper.news.models.data_transfer_objects.EverythingDto;
+import scraper.news.models.data_transfer_objects.SourcesDto;
+import scraper.news.models.data_transfer_objects.TopHeadlinesDto;
 
 @RestController
 public class EndpointController 
 {
     private String apiEndpointUrl;
+    private EndpointDto endpointData;
 
     @PostMapping("/api/v1/news/post-endpoint-data")
-    public ResponseEntity<String> postEndpointData(@RequestBody EndpointDto endpointData)
+    public ResponseEntity<String> postEndpointData(@RequestBody EndpointDto endpointData)   // Rename EndpointDto to EndpointDataDto? or something similar.
     { 
-        BaseEndpointService endpoint = CreateEndpointService.create(endpointData);
-        apiEndpointUrl = endpoint.getApiEndpointUrl();
-        System.out.println(apiEndpointUrl);
         // ✓ Call method to get NewsAPI JSON.
         // X Parse JSON for article urls, article thumbnails, etc.
         // X Scrape each article (either parse the HTML to get the article body or send the entire HTML to C# to summarize the article based off of that).
-        String responseString = getEndpoint();
-        System.out.println(responseString);
+        this.endpointData = endpointData;
+        BaseEndpointService endpoint = CreateEndpointService.create(endpointData);
+        apiEndpointUrl = endpoint.getApiEndpointUrl();
+        
+        // ApiResponse responseString = getEndpointResponse(endpointData);
+        
+        // System.out.println(apiEndpointUrl);
+        // System.out.println(responseString);
         return ResponseEntity.ok("Successful post! Make summary call to C# and return here eventually. " + apiEndpointUrl);
     }
 
-    // Gets NewsAPI endpoint JSON.
-    private String getEndpoint()
-    {
-        String responseString = HttpService.getResponse(apiEndpointUrl);
-        return responseString;
-    }
-
-    // Most likely remove these GET methods and replace with methods that take a url.
     @GetMapping("/api/v1/news/everything")
-    public String everything()
+    public ResponseEntity<EverythingDto> getEverythingResponse()
     {
-        EverythingEndpointService x = new EverythingEndpointService.Builder()
-            // Use URL encoding to add "%20" between spaces (ex. department of justice -> department%20of%20justice).
-            // Wrap q in double quotes for exact phrase.
-            // Can also use URL encoding to add double quotes (ex. united states -> %22united%20states%22).
-            .q("trump")    // Add another boolean parameter, exactPhrase. Or detect if the user types double quotes and convert that if needed (ex. "\"south korea\"")
-            .searchIn("title")
-            .sources("associated-press")
-            .domains("apnews.com, nbcnews.com")
-            .excludeDomains("foxnews.com")
-            .from("2026-06-10")
-            .to("2026-06-12")
-            .language("en")
-            .sortBy(null)
-            .pageSize("10")
-            .page("1")
-            .build();
-
-        return HttpService.getResponse(x.getApiEndpointUrl());
+        isValidEndpoint("everything");
+        return ResponseEntity.ok(HttpService.getEverythingResponse(apiEndpointUrl));
     }
 
     @GetMapping("/api/v1/news/top-headlines")
-    public String topHeadlines()
+    public ResponseEntity<TopHeadlinesDto> getTopHeadlinesResponse()
     {
-        TopHeadlinesEndpointService y = new TopHeadlinesEndpointService.Builder()
-            .country("US")
-            .category("general")
-            // .sources("associated-press") // Can't mix the sources parameter with the country or category parameters.
-            .q("trump")
-            .pageSize("10")
-            .page("1")
-            .build();
-
-        return HttpService.getResponse(y.getApiEndpointUrl());
+        isValidEndpoint("top-headlines");
+        return ResponseEntity.ok(HttpService.getTopHeadlinesResponse(apiEndpointUrl));
     }
 
     @GetMapping("/api/v1/news/top-headlines/sources")
-    public String sources()
+    public ResponseEntity<SourcesDto> getSourcesResponse()
     {
-        SourcesEndpointService z = new SourcesEndpointService.Builder()
-            .category("sports")
-            .language("en")
-            .country("us")
-            .build();
-
-        return HttpService.getResponse(z.getApiEndpointUrl());
+        isValidEndpoint("top-headlines/sources");
+        return ResponseEntity.ok(HttpService.getSourcesResponse(apiEndpointUrl));
     }
+
+    // Checks to see if the endpoint type matches the method being called.
+    private void isValidEndpoint(String endpointType)
+    {
+        if (endpointData.getEndpoint().equals(endpointType) == false)
+        {
+            throw new RuntimeException("Invalid endpoint type.");
+        }
+    }
+
+    // private ApiResponse getEndpointResponse(EndpointDto endpointData)
+    // {
+
+    //     switch (endpointData.getEndpoint())
+    //     {
+    //         case "everything":
+    //             return HttpService.getEverythingResponse(apiEndpointUrl);
+    //         case "top-headlines":
+    //             return HttpService.getTopHeadlinesResponse(apiEndpointUrl);
+    //         case "top-headlines/sources":
+    //             return HttpService.getSourcesResponse(apiEndpointUrl);
+    //         default:
+    //             throw new RuntimeException("Invalid endpoint type.");
+    //     }
+    //     // String responseString = HttpService.getNewsApiResponse(apiEndpointUrl);
+    //     // return responseString;
+    // }
+
+    // // Most likely remove these GET methods and replace with methods that take a url.
+    // @GetMapping("/api/v1/news/everything")
+    // public String everything()
+    // {
+    //     EverythingEndpointService x = new EverythingEndpointService.Builder()
+    //         // Use URL encoding to add "%20" between spaces (ex. department of justice -> department%20of%20justice).
+    //         // Wrap q in double quotes for exact phrase.
+    //         // Can also use URL encoding to add double quotes (ex. united states -> %22united%20states%22).
+    //         .q("trump")    // Add another boolean parameter, exactPhrase. Or detect if the user types double quotes and convert that if needed (ex. "\"south korea\"")
+    //         .searchIn("title")
+    //         .sources("associated-press")
+    //         .domains("apnews.com, nbcnews.com")
+    //         .excludeDomains("foxnews.com")
+    //         .from("2026-06-10")
+    //         .to("2026-06-12")
+    //         .language("en")
+    //         .sortBy(null)
+    //         .pageSize("10")
+    //         .page("1")
+    //         .build();
+
+    //     return HttpService.getResponse(x.getApiEndpointUrl());
+    // }
+
+    // @GetMapping("/api/v1/news/top-headlines")
+    // public String topHeadlines()
+    // {
+    //     TopHeadlinesEndpointService y = new TopHeadlinesEndpointService.Builder()
+    //         .country("US")
+    //         .category("general")
+    //         // .sources("associated-press") // Can't mix the sources parameter with the country or category parameters.
+    //         .q("trump")
+    //         .pageSize("10")
+    //         .page("1")
+    //         .build();
+
+    //     return HttpService.getResponse(y.getApiEndpointUrl());
+    // }
+
+    // @GetMapping("/api/v1/news/top-headlines/sources")
+    // public String sources()
+    // {
+    //     SourcesEndpointService z = new SourcesEndpointService.Builder()
+    //         .category("sports")
+    //         .language("en")
+    //         .country("us")
+    //         .build();
+
+    //     return HttpService.getResponse(z.getApiEndpointUrl());
+    // }
 
     @PostMapping("/api/v1/news/summary")
     public ResponseEntity<String> summarization(@RequestBody ArticleTextDto articleText)
