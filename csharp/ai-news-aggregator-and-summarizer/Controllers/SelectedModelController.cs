6ede14bb6ff0c09e9e360a54a3f7d89e970eb6b8
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-20 19:56:22 -0400

    Create new GET and POST endpoints to change local Ollama model

---
+++ csharp/ai-news-aggregator-and-summarizer/Controllers/SelectedModelController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using ai_news_aggregator_and_summarizer.Services;
+using ai_news_aggregator_and_summarizer.Models;
+
+namespace ai_news_aggregator_and_summarizer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SelectedModelController : ControllerBase
+{
+    // Return IActionResult, ActionResult, or IEnumerable?
+    [HttpGet]
+    public OllamaModelsDto GetLocalModels()
+    {
+        // Figure out a way to prevent controllers from running if Ollama isn't running.
+        return new OllamaModelsDto();
+    }
+
+    [HttpPost]
+    public string PostSelectedModel([FromBody] string newModel)
+    {
+        int offset = 3;
+        int start = newModel.IndexOf(": ") + offset;
+        int end = newModel.IndexOf("\"}");
+
+        newModel = newModel.Substring(0, end);
+        newModel = newModel.Substring(start);
+
+        if (SummaryService.IsModelValid(newModel) == false)
+        {
+            return $"Invalid Ollama model selected.";
+        }
+
+        SummaryService.SelectedModel = newModel;
+        return $"Successfully changed Ollama model to {newModel}.";
+    }
+}
