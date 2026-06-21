31129854c1acb1e0bc1fcd021976500d093a4a7f
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-20 20:02:07 -0400

    Update file

--- java/newsscraper/src/main/java/scraper/news/Main.java
+++ java/newsscraper/src/main/java/scraper/news/Main.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import scraper.news.services.HttpService;
-import scraper.news.services.parsers.Parser;
 
 @SpringBootApplication
 public class Main 
@@ -20,26 +15,6 @@ public class Main
         // .\/mvnw.cmd spring-boot:run
         // To run from terminal, use "mvn spring-boot:run" in the newsscraper directory.
         // Spring Boot
-        SpringApplication.run(Main.class, args);
-
-        // HttpService.getWebsiteResponse("https://www.buzzfeed.com/kristenharris1/tell-us-about-the-worst-celebrity-memoir-youve-ever-read?origin=web-hf");
-
-        // ApNewsParser x = new ApNewsParser("<html><head><title>First parse</title></head>");
-        // ApNewsParser x = new ApNewsParser("https://apnews.com/article/g7-iran-ukraine-trump-macron-zelenskyy-e7fad4eabaae8181f70fa5a0b9e499b2");
-
-        // System.out.println(summarize("https://www.axios.com/2026/06/16/anthropic-fable-trump-white-house-cybersecurity"));
+        SpringApplication.run(Main.class, args);    // Implement Swagger API docs?
     }
-
-    // public static String summarize(String url) 
-    // {     
-    //     String websiteHtml = getWebsiteHtml(url);
-    //     String pageContent = Parser.parse(url, websiteHtml);
-    //     String summary = HttpService.getSummarization(pageContent);
-    //     return summary;
-    // }
-
-    // private static String getWebsiteHtml(String url)
-    // {
-    //     return HttpService.getWebsiteResponse(url);
-    // }
 }
