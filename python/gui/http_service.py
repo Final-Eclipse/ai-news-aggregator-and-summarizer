a390817ca5e36b18e07b06628eea8f6d7bd1efd3
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-24 05:22:17 -0400

    Update file

--- python/gui/http_service.py
+++ python/gui/http_service.py
@@ -2,16 +2,22 @@ import requests, asyncio, aiohttp
 from ollama_models_dto import OllamaModelsDto
 
 class HttpService():
-    @staticmethod
-    async def get_local_ollama_models() -> dict:
-        """Return a new OllamaModelsDto that contains all local Ollama models installed."""
-        async with aiohttp.ClientSession() as session:
-            async with session.get("http://localhost:8080/api/v1/models/ollama") as response:
-                local_models: dict = await response.json()
+    # @staticmethod
+    # async def get_local_ollama_models() -> dict:
+    #     """Return a new OllamaModelsDto that contains all local Ollama models installed."""
+    #     async with aiohttp.ClientSession() as session:
+    #         async with session.get("http://localhost:8080/api/v1/models/ollama") as response:
+    #             local_models: dict = await response.json()
         
-        return local_models
+    #     return local_models
 
+    # @staticmethod
+    # def main():
+    #     loop = asyncio.get_event_loop()
+    #     local_models = loop.run_until_complete(HttpService.get_local_ollama_models())
+    #     print(local_models)
+    #     return local_models
+    
     @staticmethod
-    def main():
-        loop = asyncio.get_event_loop()
-        return loop.run_until_complete(HttpService.get_local_ollama_models())
+    def get_local_ollama_models():
+        return OllamaModelsDto(requests.get("http://localhost:8080/api/v1/models/ollama").json())
