4e59319775d03781eb2c5639db629f11eb427858
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-29 05:17:26 -0400

    Create method to make POST request to update endpoint data

--- python/gui/worker.py
+++ python/gui/worker.py
@@ -1,14 +1,29 @@
 from PyQt5.QtCore import QObject, pyqtSignal
 from ollama_models_dto import OllamaModelsDto
 from http_service import HttpService
+import requests
 
 
 class Worker(QObject):
+    # Global
     finished = pyqtSignal()
+
+    # Local models
     local_models = pyqtSignal(OllamaModelsDto)
 
-    def get_models(self):
+    # Send post request
+    post_request_finished = pyqtSignal(str)
+
+    def get_models(self) -> str:
         result = HttpService.get_local_ollama_models()
    
         self.local_models.emit(result)
         self.finished.emit()
+
+    def send_post_request(self, query_parameters) -> str:
+        headers = {"Content-Type": "application/json"}
+        request = requests.post(f"http://localhost:8080/api/v1/news/post-endpoint-data", data=query_parameters, headers=headers)
+        
+        self.finished.emit()
+        self.post_request_finished.emit(request.text)
+        
