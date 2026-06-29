74a7ce757da2f16307ff4252a81f45a564193343
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-29 05:22:14 -0400

    Add methods to assemble query parameters in JSON format

--- python/gui/endpoint.py
+++ python/gui/endpoint.py
@@ -1,11 +1,62 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout
+from PyQt5.QtWidgets import QComboBox, QLineEdit, QWidget, QLabel, QGridLayout
 from PyQt5.QtCore import Qt
+import json
+from helpers import Language, Country
 
 class Endpoint():
     def __init__(self):
         self.parameters: dict
         self.layout: QGridLayout 
         self.container: QWidget
+
+    def _convert_language_to_iso_code(self, language) -> str:
+        if language in Language.codes: 
+            return Language.codes[language] 
+        else:
+            return None
+        
+    def _convert_country_to_iso_code(self, country) -> str:
+        if country in Country.codes:
+            return Country.codes[country]
+        else:
+            return None
+
+    def _get_widget_text(self, widget) -> str:
+        text: str = ""
+
+        if type(widget) == QComboBox:
+            if widget.currentIndex() != 0:
+                text = widget.currentText()
+
+        elif type(widget) == QLineEdit:
+            text = widget.text()
+
+        return text
+
+    def get_json(self, endpoint_type) -> str:
+        json_dict = {}
+        json_dict["endpoint"] = endpoint_type
+
+        for key, widget in self.parameters.items():
+            text = self._get_widget_text(widget)
+
+            language_code = self._convert_language_to_iso_code(text)
+            if language_code != None:
+                text = language_code
+            
+            country_code = self._convert_country_to_iso_code(text)
+            if country_code != None:
+                text = country_code
+
+            if text == "":
+                text = None
+
+            if text != None:
+                text = text.replace(" ", "-")
+
+            json_dict[key] = text
+
+        return json.dumps(json_dict)
 
     def _create_layout(self) -> QGridLayout: 
         layout = QGridLayout()
