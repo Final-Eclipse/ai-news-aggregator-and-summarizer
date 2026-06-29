811094ea8f219d8e48856d65b18e27baf3dfbf84
Final-Eclipse <AnimeAndManga1@proton.me>
2026-06-29 05:24:54 -0400

    Create helper class

---
+++ python/gui/helpers/country.py
@@ -0,0 +1,11 @@
+class Country():
+    codes = {
+        "United States": "us"
+    }
+
+    qcombobox_options = ["Select country", "United States"]
+
+"""
+ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za.
+https://newsapi.org/docs/endpoints/sources
+"""
---
+++ python/gui/helpers/language.py
@@ -0,0 +1,35 @@
+class Language():
+    # ar de en es fr he it nl no pt ru sv ud zh.
+    codes = {
+        "Arabic": "ar",
+        "German": "de",
+        "English": "en",
+        "Spanish": "sp",
+        "French": "fr",
+        "Hebrew": "he",
+        "Italian": "it",
+        "Dutch": "nl",
+        "Norwegian": "no",
+        "Portuguese": "pt",
+        "Russian": "ru",
+        "Swedish": "sv",
+        "Chinese": "zh"
+        # ud not a language?
+    }
+
+    qcombobox_options = [
+        "Select language",
+        "Arabic",
+        "German",
+        "English",
+        "Spanish",
+        "French",
+        "Hebrew",
+        "Italian",
+        "Dutch",
+        "Norwegian",
+        "Portuguese",
+        "Russian",
+        "Swedish",
+        "Chinese"
+    ]
