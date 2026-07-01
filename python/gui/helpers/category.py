bd7a39f51bbcf3baa24c7a81e28372f4291d23af
Final-Eclipse <AnimeAndManga1@proton.me>
2026-07-01 06:58:50 -0400

    Update file

--- python/gui/helpers/category.py
+++ python/gui/helpers/category.py
@@ -1,3 +1,3 @@
 class Category():
     options = ["Business", "Entertainment", "General", "Health", "Science", "Sports", "Technology"]
-    qcombobox_options = ["Select category", "Business", "Entertainment", "General", "Health", "Science", "Sports", "Technology"]
+    qcombobox_options = ["Select category"] + options
--- python/gui/helpers/language.py
+++ python/gui/helpers/language.py
@@ -1,5 +1,4 @@
 class Language():
-    # ar de en es fr he it nl no pt ru sv ud zh.
     codes = {
         "Arabic": "ar",
         "German": "de",
@@ -14,22 +13,6 @@ class Language():
         "Russian": "ru",
         "Swedish": "sv",
         "Chinese": "zh"
-        # ud not a language?
     }
 
-    qcombobox_options = [
-        "Select language",
-        "Arabic",
-        "German",
-        "English",
-        "Spanish",
-        "French",
-        "Hebrew",
-        "Italian",
-        "Dutch",
-        "Norwegian",
-        "Portuguese",
-        "Russian",
-        "Swedish",
-        "Chinese"
-    ]
+    qcombobox_options = ["Select language"] + [code for code in codes]
