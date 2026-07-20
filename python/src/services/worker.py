1a531fee4a7265d22b47ed4411332a504ce7c00d
Final-Eclipse <AnimeAndManga1@proton.me>
2026-07-20 05:12:08 -0400

    Implement classes for posting endpoint data and retrieving it

--- python/src/services/worker.py
+++ python/src/services/worker.py
@@ -1,28 +1,58 @@
-from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtCore import QObject, pyqtSignal, QThread, QRunnable, pyqtSlot
 from services.http_service import HttpService
 import requests
 
 
-class Worker(QObject):
-    # Global
-    finished = pyqtSignal()
+# class Worker(QObject):
+#     # Global
+#     finished = pyqtSignal()
 
-    # Local models
-    local_models = pyqtSignal(dict)
+#     # Local models
+#     local_models = pyqtSignal(dict)
 
-    # Send post request
-    post_request_finished = pyqtSignal(str)
+#     # Send post request
+#     post_request_finished = pyqtSignal(str)
 
-    def get_models(self) -> str:
-        result = HttpService.get_local_ollama_models()
+#     # Get json response
+#     json_response_finished = pyqtSignal(dict)
+
+#     def get_models(self) -> str:
+#         result = HttpService.get_local_ollama_models()
    
-        self.local_models.emit(result)
-        self.finished.emit()
+#         self.local_models.emit(result)
+#         self.finished.emit()
 
-    def send_post_request(self, query_parameters) -> str:
+#     def post_endpoint_data(self, query_parameters) -> str:
+#         headers = {"Content-Type": "application/json"}
+#         request = requests.post(f"http://localhost:8080/api/v1/news/post-endpoint-data", data=query_parameters, headers=headers)
+        
+#         self.finished.emit()
+#         self.post_request_finished.emit(request.text)
+    
+#     def get_json_response(self) -> dict:
+#         request = requests.get("http://localhost:8080/api/v1/news/everything").json()
+#         self.json_response_finished.emit(request)
+
+class EndpointDataWorker(QRunnable):
+    @pyqtSlot()
+    def run(self):
         headers = {"Content-Type": "application/json"}
-        request = requests.post(f"http://localhost:8080/api/v1/news/post-endpoint-data", data=query_parameters, headers=headers)
-        
-        self.finished.emit()
-        self.post_request_finished.emit(request.text)
-        
+        request = requests.post(f"http://localhost:8080/api/v1/news/post-endpoint-data", data=self.query_parameters, headers=headers)
+
+    def set_query_parameters(self, query_parameters):
+        self.query_parameters = query_parameters
+
+class EndpointResponseWorker(QRunnable):  
+    def __init__(self):
+        super().__init__()
+        self.signal = Signals()
+
+    @pyqtSlot()
+    def run(self):
+        request = requests.get("http://localhost:8080/api/v1/news/everything")  # Change endpoint depending on endpoint data.
+        response = request.json()
+
+        self.signal.finished.emit(response)
+
+class Signals(QObject):
+    finished = pyqtSignal(dict)
