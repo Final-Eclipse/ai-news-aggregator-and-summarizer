356b02f1998df6d55a108e294b015c66f50a9ba9
Final-Eclipse <AnimeAndManga1@proton.me>
2026-08-08 22:12:42 -0400

    feat: create class Localhosts(QRunnable)

--- python/src/services/localhosts.py
+++ python/src/services/localhosts.py
@@ -1,41 +1,51 @@
 import asyncio
 from pathlib import Path
+from PyQt5.QtCore import QRunnable, pyqtSlot
 
-async def _run_dotnet() -> None:
-    """Start the ASP.NET Core dotnet localhost."""
-    process = await asyncio.create_subprocess_shell(
-        cmd="dotnet run",
-        cwd=f"{Path.cwd().parent}/csharp/ai-news-aggregator-and-summarizer"
-    )
+class Localhosts(QRunnable):
+    def __init__(self):
+        super().__init__()
 
-    await process.communicate()
+    @pyqtSlot()
+    async def run(self) -> None:
+        """Call and start all localhosts."""
+        tasks = [
+            Localhosts._run_dotnet(), 
+            Localhosts._run_java(), 
+            Localhosts._run_ollama()
+        ]
+        
+        await asyncio.gather(*tasks)
 
-async def _run_java() -> None:
-    """Start the Java Spring Boot localhost."""
-    process = await asyncio.create_subprocess_shell(
-        cmd="mvnw.cmd spring-boot:run",
-        cwd=f"{Path.cwd().parent}/java/newsscraper"
-    )
+    @staticmethod
+    async def _run_dotnet() -> None:
+        """Start the ASP.NET Core dotnet localhost."""
+        process = await asyncio.create_subprocess_shell(
+            cmd="dotnet run",
+            cwd=f"{Path.cwd().parent}/csharp/ai-news-aggregator-and-summarizer"
+        )
 
-    await process.communicate()
+        await process.communicate()
 
-async def _run_ollama() -> None:
-    """Start the Ollama localhost."""
-    process = await asyncio.create_subprocess_shell(
-        cmd="ollama serve"
-    )
+    @staticmethod
+    async def _run_java() -> None:
+        """Start the Java Spring Boot localhost."""
+        process = await asyncio.create_subprocess_shell(
+            cmd="mvnw.cmd spring-boot:run",
+            cwd=f"{Path.cwd().parent}/java/newsscraper"
+        )
 
-    await process.communicate()
+        await process.communicate()
 
-async def main() -> None:
-    """Call and start all localhosts."""
-    tasks = [
-        _run_dotnet(), 
-        _run_java(), 
-        _run_ollama()
-    ]
-    
-    await asyncio.gather(*tasks)
+    @staticmethod
+    async def _run_ollama() -> None:
+        """Start the Ollama localhost."""
+        process = await asyncio.create_subprocess_shell(
+            cmd="ollama serve"
+        )
+
+        await process.communicate()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    localhosts = Localhosts()
+    asyncio.run(localhosts.run())
