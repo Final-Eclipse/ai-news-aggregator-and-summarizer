bd1298d825888257ad5e30b97064d9f9846352b0
Final-Eclipse <AnimeAndManga1@proton.me>
2026-08-08 22:39:44 -0400

    feat: change asyncio -> subprocess

--- python/src/services/localhosts.py
+++ python/src/services/localhosts.py
@@ -1,51 +1,38 @@
 import asyncio
 from pathlib import Path
 from PyQt5.QtCore import QRunnable, pyqtSlot
+import subprocess
 
 class Localhosts(QRunnable):
     def __init__(self):
         super().__init__()
 
+        self.processes = []
+
     @pyqtSlot()
-    async def run(self) -> None:
-        """Call and start all localhosts."""
-        tasks = [
-            Localhosts._run_dotnet(), 
-            Localhosts._run_java(), 
-            Localhosts._run_ollama()
-        ]
-        
-        await asyncio.gather(*tasks)
+    def run(self) -> None:
+        self._run_java()
+        self._run_dotnet()
+        self._run_ollama()
 
-    @staticmethod
-    async def _run_dotnet() -> None:
-        """Start the ASP.NET Core dotnet localhost."""
-        process = await asyncio.create_subprocess_shell(
-            cmd="dotnet run",
-            cwd=f"{Path.cwd().parent}/csharp/ai-news-aggregator-and-summarizer"
-        )
+    def _run_java(self) -> None:
+        process = subprocess.Popen(["mvnw.cmd", "spring-boot:run"], text=True, cwd=f"{Path.cwd().parent}/java/newsscraper", shell=True)
+        self.processes.append(process)
 
-        await process.communicate()
+    def _run_dotnet(self) -> None:
+        process = subprocess.Popen(["dotnet", "run"], text=True, cwd=f"{Path.cwd().parent}/csharp/ai-news-aggregator-and-summarizer", shell=True)
+        self.processes.append(process)
 
-    @staticmethod
-    async def _run_java() -> None:
-        """Start the Java Spring Boot localhost."""
-        process = await asyncio.create_subprocess_shell(
-            cmd="mvnw.cmd spring-boot:run",
-            cwd=f"{Path.cwd().parent}/java/newsscraper"
-        )
+    def _run_ollama(self) -> None:
+        process = subprocess.Popen(["ollama", "serve"], text=True, shell=True)
+        self.processes.append(process)
 
-        await process.communicate()
-
-    @staticmethod
-    async def _run_ollama() -> None:
-        """Start the Ollama localhost."""
-        process = await asyncio.create_subprocess_shell(
-            cmd="ollama serve"
-        )
-
-        await process.communicate()
+    def stop(self) -> None:
+        for process in self.processes:
+            process: subprocess.Popen
+            process.kill() 
+            print("Process terminated.")
 
 if __name__ == "__main__":
-    localhosts = Localhosts()
-    asyncio.run(localhosts.run())
+    x = Localhosts()
+    x.run()
