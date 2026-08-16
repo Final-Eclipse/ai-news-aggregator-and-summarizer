85842e1c11e51ba142c868bb3dff756754313af6
Final-Eclipse <AnimeAndManga1@proton.me>
2026-08-15 23:19:03 -0400

    feat(pagination): fetch thumbnails asynchronously
     - Reworked GUI update system into a series of new signals and slots
     - Renamed various methods and variables
     - Created ThumbnailWorker() and ThumbnailHandler() classes
     - Removed ability to move from last page to first
     - Merged page container and layout creation methods
     - Removed requests.get() from _create_thumbnails() method, moved to
       ThumbnailWorker() class
     - Added functionality to closeEvent() method, now clears the global
       instance of QThreadPool() on application close

--- python/src/main_display.py
+++ python/src/main_display.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QComboBox, QGridLayout, QHBoxLayout, QMainWindow, QPushButton, QSizePolicy, QWidget, QLabel, QVBoxLayout, QLineEdit, QStackedWidget
-from PyQt5.QtCore import QObject, QSize, QThread, QThreadPool, Qt, pyqtSignal, QRunnable
+from PyQt5.QtCore import QObject, QSize, QThread, QThreadPool, Qt, pyqtSignal, QRunnable, pyqtSlot, QUrl
 from PyQt5.QtGui import QFontMetrics, QFont, QPixmap
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
 import requests
 from pathlib import Path
 from PyQt5.QtCore import QLoggingCategory
@@ -17,8 +18,12 @@ class MainDisplay(QMainWindow):
         super().__init__()
 
         self.signals = Signals()
+        self.signals.thumbnails_finished.connect(lambda thumbnails: self._create_news_card_containers(thumbnails))
+        self.signals.news_cards_finished.connect(lambda news_cards: self._divide_news_cards_into_pages(news_cards))
+        self.signals.page_division_finished.connect(lambda pages: self._add_pages_to_container(pages))
 
         self.container = QStackedWidget()
+        self.desc_containers: list[QWidget]
 
         self.landing_page: QLabel
         self._init_landing_page()
@@ -30,7 +35,7 @@ class MainDisplay(QMainWindow):
         self.articles_per_page = 4
 
         # Number of pages to get at one time.
-        self.pages_to_get = 5
+        self.pages_to_get = 2
 
         # The current page number the user is on.
         self.current_page_number = 1
@@ -51,19 +56,26 @@ class MainDisplay(QMainWindow):
         self.landing_page.deleteLater()
 
     def next_page(self) -> None:
-        """Change the current page of the container to the next."""
-        self._change_page(self.next_index(reverse=False))
+        """
+        Change the current page of the container to the next.
+        
+        Does not move from the last page to the first page.
+        """
+        if self.current_page_number == self._get_container_children():
+            return
+        
+        self._change_page(self._next_index(reverse=False))
 
     def previous_page(self) -> None:
         """
         Change the current page of the container to the previous.
         
-        Does not move from page 1 to the last page.
+        Does not move from the first page to the last page.
         """
         if self.current_page_number == 1:
             return
         
-        self._change_page(self.next_index(reverse=True))
+        self._change_page(self._next_index(reverse=True))
 
     def _change_page(self, new_page_index: int) -> None:
         """
@@ -79,7 +91,7 @@ class MainDisplay(QMainWindow):
         @param new_page_number: Integer value of the new page index.
         """
         self.container.setCurrentIndex(new_page_index)
-
+        
         # Create more pages if current page reaches a certain number.
         if self._get_container_children() - 2 <= new_page_index:
             self.update(reset_pages=False)
@@ -96,8 +108,7 @@ class MainDisplay(QMainWindow):
         if reset_pages == True:
             self._delete_previous_pages()
 
-        pages: list[list[QWidget]] = self._assemble_pages(self.articles)
-        self._add_pages_to_container(pages)
+        self._get_thumbnails_urls_and_descs(self.articles)
         
         # Remove landing page.
         if self.landing_page in self.container.children():
@@ -124,55 +135,61 @@ class MainDisplay(QMainWindow):
         """
         Add pages to container.
 
-        @param news_cards: List of QWidget lists of pages.
+        @param pages: List of QWidget lists of pages.
         """
-        # Create page container and add to main display container.
         for page in pages:
-            page: list[QWidget]
-            page_layout = self._create_layout(page)
-            page_container = self._create_page_container(page_layout)
+            page_container: QWidget = self._create_page_container(page)
             self.container.addWidget(page_container)
 
-    def _assemble_pages(self, articles: list) -> list[list[QWidget]]:
+    def _get_thumbnails_urls_and_descs(self, articles: list) -> None:
         """
-        Assemble news cards into pages.
+        Get thumbnail URLs and create description containers.
 
         @param articles: List of articles.
-        @return: QWidget list of pages.
         """
-        news_cards = []
-        pages = []
+        desc_containers = []
+        thumbnail_urls = []
         for article in articles[self.article_index:]:
-            # Create thumbnail.
+            # Get thumbnail urls.
             thumbnail_url: str = article[6]
-            thumbnail: QLabel = self._create_thumbnail(thumbnail_url)
+            thumbnail_urls.append(thumbnail_url)
 
             # Create description container.
             news_outlet: str = article[1]
             title: str = article[3]
             author: str = article[2]
             desc_container: QWidget = self._create_desc_container(news_outlet, title, author)
-
-            # Create news card.
-            news_card: QWidget = self._create_news_card_container(thumbnail, desc_container)
-            news_cards.append(news_card)
-
-            if len(news_cards) == self.articles_per_page:
-                pages.append(news_cards.copy())
-                news_cards.clear()
+            desc_containers.append(desc_container)
 
             # Prevents looping over entire list, which can take a lot of time when the user most likely won't go to the last page anyways.
-            if len(pages) == self.pages_to_get:
+            if len(desc_containers) == self.pages_to_get * self.articles_per_page:
+                self.desc_containers = desc_containers
                 break
 
             self.article_index += 1
         self.article_index += 1
 
-        # If there are still articles left that amount to less than self.articles_per_page, add the rest of them.
-        if len(news_cards) > 0:
-            pages.append(news_cards.copy())
+        self._convert_thumbnails_to_bytes(thumbnail_urls)
 
-        return pages
+    def _convert_thumbnails_to_bytes(self, thumbnail_urls: list[str]) -> None: 
+        """
+        Convert all given thumbnail URLs to their bytes form.
+
+        Create threads within QThreadPool to asynchronously make HTTP requests to gather
+        bytes data without disrupting the main GUI thread.
+
+        @param thumbnail_urls: List of URLs that direct to an article's thumbnail.
+        """
+        list_size = len(thumbnail_urls)
+        handler = ThumbnailHandler(list_size)
+        handler.signal.threads_finished.connect(lambda bytes_data: self._create_thumbnails(bytes_data))
+
+        for index_position, url in enumerate(thumbnail_urls):
+            threadpool = QThreadPool().globalInstance()
+            worker = ThumbnailWorker(url, index_position)
+            worker.signal.non_bytes_finished.connect(lambda data, pos: handler._handle_thumbnail_urls(data, pos))
+            worker.signal.bytes_finished.connect(lambda data, pos: handler._handle_thumbnail_urls(data, pos))
+            threadpool.start(worker)
 
     def _get_container_children(self) -> int:
         """
@@ -184,7 +201,7 @@ class MainDisplay(QMainWindow):
         """
         return len(self.container.children()) - 1 # Excludes QStackedLayout.
 
-    def next_index(self, reverse: bool) -> int:
+    def _next_index(self, reverse: bool) -> int:
         """
         Returns the next/previous valid index position in the QStackedWidget.
         
@@ -209,50 +226,60 @@ class MainDisplay(QMainWindow):
         
         return next_index
 
-    def _create_page_container(self, layout: QVBoxLayout) -> QWidget:
+    def _create_page_container(self, page: list[QWidget]) -> QWidget:
         """
-        Creates a page that contains a layout with articles.
+        Create a page that contains a layout with articles.
 
         @return: QWidget page.
         """
+        layout = QVBoxLayout()
+        for news_card in page:
+            layout.addWidget(news_card)
+
         page = QWidget()
         page.setLayout(layout)
         return page
 
-    def _create_layout(self, news_cards: list) -> QVBoxLayout:
-        """
-        Creates a layout of news cards.
+    def _divide_news_cards_into_pages(self, news_cards: list[QWidget]) -> None:
+        current_page: list[QWidget] = []
+        pages: list[list[QWidget]] = []
+        for news_card in news_cards:
+            current_page.append(news_card)
 
-        @param news_cards: List of news cards.
-        @return: QVBoxLayout of news cards.
-        """
-        layout = QVBoxLayout()
-        for card in news_cards:
-            layout.addWidget(card)
+            if len(current_page) == self.articles_per_page:
+                pages.append(current_page.copy())
+                current_page.clear()
 
-        return layout
-
-    def _create_news_card_container(self, thumbnail: QLabel, desc_container: QWidget) -> QWidget:
-        """
-        Creates a news card that contains an article's description and thumbnail.
-
-        @param thumbnail: The picture an article comes with.
-        @param desc_container: The description of an article containing the news outlet, title, and author.
-        @return: QWidget news card.
-        """
-        container = QWidget()
-
-        layout = QHBoxLayout()
-        layout.addWidget(thumbnail)
-        layout.addWidget(desc_container, alignment=Qt.AlignmentFlag.AlignLeft)
-
-        width = int(thumbnail.width() + desc_container.width())
-        height = int(thumbnail.height() + desc_container.height())
+        # If there are still articles left that amount to less than self.articles_per_page, add the rest of them.
+        if len(current_page) > 0:
+            pages.append(current_page.copy())
+            current_page.clear()
         
-        container.setLayout(layout)
-        container.setMaximumSize(width, height)
+        self.signals.page_division_finished.emit(pages)
 
-        return container
+    def _create_news_card_containers(self, thumbnails: list[QLabel]) -> None:
+        """
+        Create news cards that contain an article's description and thumbnail.
+
+        @param thumbnails: The picture an article comes with.
+        """
+        news_cards: list[QWidget] = []
+        for x in range(0, len(thumbnails)):
+            container = QWidget()
+
+            layout = QHBoxLayout()
+            layout.addWidget(thumbnails[x])
+            layout.addWidget(self.desc_containers[x], alignment=Qt.AlignmentFlag.AlignLeft)
+
+            width = int(thumbnails[x].width() + self.desc_containers[x].width())
+            height = int(thumbnails[x].height() + self.desc_containers[x].height())
+            
+            container.setLayout(layout)
+            container.setMaximumSize(width, height)
+
+            news_cards.append(container)
+
+        self.signals.news_cards_finished.emit(news_cards)
 
     def _create_desc_container(self, news_outlet: str, title: str, author: str) -> QWidget:
         """
@@ -289,32 +316,33 @@ class MainDisplay(QMainWindow):
         
         return container
 
-    def _create_thumbnail(self, url: str) -> QLabel:
+    def _create_thumbnails(self, bytes_data: list[bytes|str]) -> None:
         """
-        Creates a thumbnail from a URL.
+        Create pixmaps and apply to thumbnails.
 
         Some images can produce a warning in the terminal from PyQt5 stating "qt.gui.icc: fromIccProfile: failed minimal tag size sanity."
         This warning is disabled in _disable_icc_warning().
 
-        @param url: Url to convert to bytes.
-        @return: QLabel thumbnail image.
+        @param bytes_data: List of bytes or str for every article's thumbnail.
         """
+        thumbnails = []
         placeholder_thumbnail_path = f"{Path.cwd()}/src/images/placeholder_thumbnail.png"
-        pixmap = QPixmap()
+        for data in bytes_data:
+            pixmap = QPixmap()
 
-        if url == "N/A":
-            pixmap.load(placeholder_thumbnail_path)
-        else:
-            pixmap_data = requests.get(url).content
-            if pixmap.loadFromData(pixmap_data) == False:
+            if data == "N/A":
+                pixmap.load(placeholder_thumbnail_path)
+            elif pixmap.loadFromData(data) == False:
                 pixmap.load(placeholder_thumbnail_path)
 
-        thumbnail = QLabel()
-        thumbnail.setScaledContents(True)
-        thumbnail.setMaximumSize(150, 150)
-        thumbnail.setPixmap(pixmap)
+            thumbnail = QLabel()
+            thumbnail.setScaledContents(True)
+            thumbnail.setMaximumSize(150, 150)
+            thumbnail.setPixmap(pixmap)
+
+            thumbnails.append(thumbnail)    
         
-        return thumbnail
+        self.signals.thumbnails_finished.emit(thumbnails)
 
     def _create_label(self, text: str) -> QLabel:
         """
@@ -331,8 +359,77 @@ class MainDisplay(QMainWindow):
         """Disable warning when loading image with an incorrect ICC color profile."""
         QLoggingCategory.setFilterRules("qt.gui.icc.warning=false\n")
 
+    def closeEvent(self, a0):
+        """Remove any unstarted runnables from the QThreadPool queue on application close."""
+        QThreadPool().globalInstance().clear()
+        return super().closeEvent(a0)
+
 class Signals(QObject):
+    # Pagination
     page_changed = pyqtSignal(int)
+
+    # Update GUI
+    thumbnails_finished = pyqtSignal(list)
+    news_cards_finished = pyqtSignal(list)
+    page_division_finished = pyqtSignal(list)
+
+class ThumbnailHandler():
+    def __init__(self, list_size: int) -> None:
+        """
+        @param max_size: List size to be used to hold the incoming bytes.
+        """
+        self.signal = ThumbnailSignals()
+        self.all_bytes_data = [None] * list_size
+        
+    def _handle_thumbnail_urls(self, bytes_data: bytes, index_position: int) -> None:
+        """
+        Store incoming bytes in a list and emit when there are no more active threads.
+        
+        @param bytes_data: Incoming bytes to be appended to this instance's list.
+        @param index_position: Position the bytes should appear in the list.
+        """
+        self.all_bytes_data[index_position] = bytes_data
+
+        active_thread_count = QThreadPool().globalInstance().activeThreadCount()        
+        if active_thread_count == 0:
+            self.signal.threads_finished.emit(self.all_bytes_data)
+
+class ThumbnailWorker(QRunnable):
+    def __init__(self, url: str, index_position: int) -> None:
+        """
+        @param url: Thumbnail URL.
+        @param index_position: Position the URL appears in the list.
+        """
+        super().__init__()
+        self.signal = ThumbnailSignals()
+        self.url = url
+        self.index_position = index_position
+
+    @pyqtSlot()
+    def run(self) -> None:
+        """
+        Make HTTP request to URL and emit the response as bytes.
+        
+        Invalid URLs are emitted immediately without any request.
+        
+        Long requests timeout after a given amount of time.
+        """
+        if self.url == "N/A":
+            self.signal.non_bytes_finished.emit(self.url, self.index_position)
+        else:
+            try:
+                bytes_data = requests.get(self.url, timeout=1).content
+                self.signal.bytes_finished.emit(bytes_data, self.index_position)
+            except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
+                self.signal.non_bytes_finished.emit("N/A", self.index_position)
+
+class ThumbnailSignals(QObject):
+    # ThumbnailHandler
+    threads_finished = pyqtSignal(list)
+
+    # ThumbnailWorker
+    bytes_finished = pyqtSignal(bytes, int)
+    non_bytes_finished = pyqtSignal(str, int)
 
 def main() -> None:  
     app = QApplication([])
